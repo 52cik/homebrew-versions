31f36164f9f914972f64161c44e2624cdead799d
楼教主 <cik520@qq.com>
2016-04-02 04:07:29 +0800

    添加 node 5.x 版本

---
+++ node5.rb
@@ -0,0 +1,13 @@
+cask 'node5' do
+  version '5.10.0'
+  sha256 '581fda48da967e8f5d3ba5ea5e1f6e12e689b91ee26393f398ab261dfe1fcfd2'
+
+  url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
+  name 'Node.js'
+  homepage 'https://nodejs.org/'
+  license :mit
+
+  pkg "node-v#{version}.pkg"
+
+  uninstall pkgutil: 'org.nodejs.node.pkg'
+end
