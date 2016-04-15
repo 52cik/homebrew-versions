828c862087d8fb8f49b006c18a2cb27efb9fb0bb
楼教主 <cik520@qq.com>
2016-04-15 11:37:41 +0800

    node 跟新

--- Casks/node4.rb
+++ Casks/node4.rb
@@ -1,5 +1,5 @@
 cask 'node4' do
-  version '4.4.2'
+  version '4.4.3'
   sha256 '5d115f4d561f92c6b26f40e7d0217e99e4255e2051337bca15489c39597d05ba'
 
   url "http://npm.taobao.org/mirrors/node/v#{version}/node-v#{version}.pkg"
--- Casks/node5.rb
+++ Casks/node5.rb
@@ -1,5 +1,5 @@
 cask 'node5' do
-  version '5.10.0'
+  version '5.10.1'
   sha256 '581fda48da967e8f5d3ba5ea5e1f6e12e689b91ee26393f398ab261dfe1fcfd2'
 
   url "http://npm.taobao.org/mirrors/node/v#{version}/node-v#{version}.pkg"
