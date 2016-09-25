f098f5302d32214578f3dc356dae42b0ce0f0c29
楼教主 <cik520@qq.com>
2016-09-25 21:25:19 +0800

    更新 node v6.6.0

--- Casks/node6.rb
+++ Casks/node6.rb
@@ -1,6 +1,6 @@
 cask 'node6' do
-  version '6.5.0'
-  sha256 'bc79776fa04870cbdb3cc874d91b22c506caa6fc218cdcf1e2538e3de2248957'
+  version '6.6.0'
+  sha256 '7a9af7bf1eb98f8c206037a0dbbcd797c5684e2570d6acc8d915351bad4a5571'
 
   url "http://npm.taobao.org/mirrors/node/v#{version}/node-v#{version}.pkg"
   name 'Node.js'
