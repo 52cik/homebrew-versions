24429a623dceba7e71f8b434cd0e9f8bba2d6723
楼教主 <cik520@qq.com>
2016-10-19 11:23:55 +0800

    update atom 1.11.2

--- Casks/atom-mac.rb
+++ Casks/atom-mac.rb
@@ -1,6 +1,6 @@
 cask 'atom-mac' do
-  version '1.11.1'
-  sha256 'da725e3b224f772d64060cd8dc75a582946913c6d8e57461a271b9cb69a3cb62'
+  version '1.11.2'
+  sha256 '931285cb1cd6989ec632f01f60fe6dd22129f255cd724bfd891c40ab6bd52194'
 
   # github.com/atom/atom was verified as official when first introduced to the cask
   # url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
