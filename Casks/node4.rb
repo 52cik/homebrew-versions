80980b0378d2e6589e905921a31da08eb7aa7e25
楼教主 <cik520@qq.com>
2017-05-03 21:01:36 +0800

    update node4,6

--- Casks/node4.rb
+++ Casks/node4.rb
@@ -1,6 +1,6 @@
 cask 'node4' do
-  version '4.8.2'
-  sha256 '25606ccd44d634b3796806e4c534456b235310e9c3bdbd0afb3e1fb5d0214371'
+  version '4.8.3'
+  sha256 '369aeccb89552c79d804eebb88d61d520986a200ef935d3c39519975ad7f4ea7'
 
   # https://npm.taobao.org/mirrors/node/latest-v4.x/SHASUMS256.txt
 
--- Casks/node6.rb
+++ Casks/node6.rb
@@ -1,6 +1,6 @@
 cask 'node6' do
-  version '6.10.2'
-  sha256 '6a6fb245648236dfd80f50b5d35b54584ad666207b41decfa965581b96d68cd9'
+  version '6.10.3'
+  sha256 '0a2c7a536f9009b9f18b10640b10ea6e5db1573104e9b7f5a80779e708c218b8'
 
   # https://npm.taobao.org/mirrors/node/latest-v6.x/SHASUMS256.txt
 
