1e6e540aaaddc8cbc8b74c1e15a8feaddcaf7a21
楼教主 <cik520@qq.com>
2017-06-28 18:24:36 +0800

    update script

--- script/node.js
+++ script/node.js
@@ -22,55 +22,10 @@ const versions = {
   },
 };
 
-
+// 匹配线上版本 hash 值
 const RE_ONLINE = /\b(\w{64})\s+node-v([\d.]+)\.pkg/m;
+// 匹配本地版本 hash 值
 const RE_LOCAL = /version\s+['"]([\d.]+)['"][\s\r\n]+sha256\s+['"](\w{64})['"]/;
-
-// 遍历更新版本
-Object.keys(versions).forEach(ver => {
-  getUrl(versions[ver].url).then((data) => { // 抓取线上版本
-    const m = data.match(RE_ONLINE);
-
-    if (m === null) {
-      return null;
-    }
-
-    // 线上版本
-    return {
-      version: m[2],
-      SHASUMS256: m[1],
-    };
-  }).then((online) => { // 抓取本地版本
-    const content = getFile(versions[ver].file);
-    const m = content.match(RE_LOCAL);
-
-    if (m === null) {
-      return null;
-    }
-
-    // 本地版本
-    return {
-      online,
-      local: {
-        version: m[1],
-        SHASUMS256: m[2],
-        content: content,
-      }
-    };
-  }).then((data) => { // 更新版本
-    // console.log(JSON.stringify(data, null, '  '));
-    if (data.online.version === data.local.version) { // 版本一致则忽略
-      return false;
-    }
-
-    const content = data.local.content
-      .replace(/version\s+['"]([\d.]+)['"]/, `version '${data.online.version}'`)
-      .replace(/sha256\s+['"](\w{64})['"]/, `sha256 '${data.online.SHASUMS256}'`);
-
-    return putFile(versions[ver].file, content);
-  }).then(() => console.log(ver, 'done!')).catch(err => console.error(err));
-});
-
 
 
 /**
@@ -82,17 +37,15 @@ Object.keys(versions).forEach(ver => {
 function getUrl(url) {
   return new Promise((resolve, reject) => {
     https.get(url, (res) => {
-      const statusCode = res.statusCode;
-
-      if (statusCode !== 200) {
-        reject(new Error(`Request Failed.\nStatus Code: ${statusCode}`));
-        return res.resume();
+      if (res.statusCode !== 200) {
+        reject(new Error(`Request Failed.\nStatus Code: ${res.statusCode}`));
+        res.resume();
+      } else {
+        let rawData = '';
+        res.setEncoding('utf8');
+        res.on('data', chunk => (rawData += chunk));
+        res.on('end', () => resolve(rawData));
       }
-
-      let rawData = '';
-      res.setEncoding('utf8');
-      res.on('data', chunk => rawData += chunk);
-      res.on('end', () => resolve(rawData));
     }).on('error', reject);
   });
 }
@@ -102,7 +55,7 @@ function getUrl(url) {
  * 加载文件
  *
  * @param {String} file
- * @returns {Promise}
+ * @returns {String}
  */
 function getFile(file) {
   return fs.readFileSync(path.join(__dirname, '..', 'Casks', file), 'utf8');
@@ -113,8 +66,64 @@ function getFile(file) {
  *
  * @param {String} file
  * @param {String} data
- * @returns {Promise}
+ * @returns {String}
  */
 function putFile(file, data) {
   return fs.writeFileSync(path.join(__dirname, '..', 'Casks', file), data, 'utf8');
 }
+
+
+// 遍历更新版本
+Object.keys(versions).forEach((ver) => {
+  getUrl(versions[ver].url)
+    .then((data) => { // 抓取线上版本
+      const m = data.match(RE_ONLINE);
+
+      if (m === null) {
+        return Promise.reject(Error(`${ver} 线上版本信息抓取失败！`));
+      }
+
+      // 线上版本
+      return {
+        version: m[2], // 最新版本号
+        SHASUMS256: m[1], // 哈希值
+      };
+    })
+    .then((online) => { // 抓取本地版本
+      const content = getFile(versions[ver].file);
+      const m = content.match(RE_LOCAL);
+
+      if (m === null) {
+        return Promise.reject(Error(`${ver} 本地文件信息匹配失败！`));
+      }
+
+      // 本地版本
+      return {
+        online,
+        local: {
+          content,
+          version: m[1],
+          SHASUMS256: m[2],
+        },
+      };
+    })
+    .then((data) => { // 更新版本
+      if (data.online.version === data.local.version) { // 版本一致则忽略
+        return false;
+      }
+
+      const content = data.local.content
+        .replace(/version\s+['"]([\d.]+)['"]/, `version '${data.online.version}'`)
+        .replace(/sha256\s+['"](\w{64})['"]/, `sha256 '${data.online.SHASUMS256}'`);
+
+      putFile(versions[ver].file, content);
+      return true;
+    })
+  .then((isUpdate) => {
+    console.log(ver, isUpdate ? 'updated!' : 'done!');
+  })
+  .catch((err) => {
+    console.log('Error:', err.message);
+  });
+});
+
