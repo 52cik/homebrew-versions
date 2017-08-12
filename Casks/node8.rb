73684ec5e0f732bc27548c3a6223245e503c118e
楼教主 <cik520@qq.com>
2017-08-12 14:06:47 +0800

    update node v8.3.0

--- Casks/node8.rb
+++ Casks/node8.rb
@@ -1,6 +1,6 @@
 cask 'node8' do
-  version '8.2.1'
-  sha256 '98971a1d66b2af304a36c8760e2dbea59c8fd173319f2dcbb76b929287232982'
+  version '8.3.0'
+  sha256 '3874de85c34f7bbdb9869e4724c08b6904ae0ff9d7560f1a1829527c711dbed0'
 
   # https://npm.taobao.org/mirrors/node/latest-v8.x/SHASUMS256.txt
 
